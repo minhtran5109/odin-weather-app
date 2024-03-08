90192747d0caec3e7d542d31987e94444bedc6cb
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-03-08 15:20:27 +1100

    first fetch to weather api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import "./style.css";
 
-const test = document.getElementById("test");
-console.log(test.innerText);
+const API_KEY = "3253784739874ed798940459240803";
+
+// default
+let location = "London";
+
+async function getWeatherData() {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getWeatherData();
