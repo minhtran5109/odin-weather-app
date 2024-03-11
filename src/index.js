13d6ababcad66aa23ba9dc9a5054bd0330b79d82
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-03-11 15:52:12 +1100

    added function to process json data from api call

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import "./style.css";
 
 const API_KEY = "3253784739874ed798940459240803";
+const basedAPI = "http://api.weatherapi.com/v1";
 
 // default
-let location = "London";
+// let location = "London";
 
-async function getWeatherData() {
+async function processJsonData(data) {
+  const jsonData = await data.json();
+  // console.log(jsonData);
+  // extracting only meaningful data to user
+  return new Promise((resolve, reject) => {
+    resolve({
+      locationData: {
+        country: jsonData.location.country,
+        localTime: jsonData.location.localtime,
+        name: jsonData.location.name,
+        region: jsonData.location.region,
+      },
+      currentWeather: jsonData.current,
+    });
+    reject(new Error("Something went wrong when retrieving the API"));
+  });
+}
+
+async function getWeatherData(location) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`
+      `${basedAPI}/current.json?key=${API_KEY}&q=${location}`
     );
-    const weatherData = await response.json();
+    const weatherData = await processJsonData(response);
     console.log(weatherData);
   } catch (error) {
     console.log(error);
   }
 }
 
-getWeatherData();
+getWeatherData("London");
